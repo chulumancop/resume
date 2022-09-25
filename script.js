be50deb505ca378e85e0dcb60b72b8e9dcada5d0
chulumancop <chulumancop@gmail.com>
2022-09-25 06:39:15 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+function hideDiv()
+{
+    var skills =  document.getElementById("Skills");
+    skills.style.display="none"
+    var education =  document.getElementById("Education");
+    education.style.display="none"
+    var work =  document.getElementById("Work_experience");
+    work.style.display="none"
+    var references =  document.getElementById("References");
+    references.style.display="none"
+    var personalInfor =  document.getElementById("PersonalInfo");
+    personalInfor.style.display="Block";
+    
+}
+function education()
+{
+    var skills =  document.getElementById("Skills");
+    skills.style.display="none";
+    var personalInfor =  document.getElementById("PersonalInfo");
+    personalInfor.style.display="none";
+    var work =  document.getElementById("Work_experience");
+    work.style.display="none";
+    var references =  document.getElementById("References");
+    references.style.display="none";
+    var education =  document.getElementById("Education");
+    education.style.display="Block";
+}
+function skills()
+{
+    var skills =  document.getElementById("Skills");
+    skills.style.display="Block";
+    var personalInfor =  document.getElementById("PersonalInfo");
+    personalInfor.style.display="none";
+    var work =  document.getElementById("Work_experience");
+    work.style.display="none";
+    var references =  document.getElementById("References");
+    references.style.display="none";
+    var education =  document.getElementById("Education");
+    education.style.display="none";
+}
+function work_exp()
+{
+    var skills =  document.getElementById("Skills");
+    skills.style.display="none"
+    var personalInfor =  document.getElementById("PersonalInfo");
+    personalInfor.style.display="none"
+    var work =  document.getElementById("Work_experience");
+    work.style.display="Block"
+    var references =  document.getElementById("References");
+    references.style.display="none"
+    var education =  document.getElementById("Education");
+    education.style.display="none"
+}
+function references()
+{
+    var skills =  document.getElementById("Skills");
+    skills.style.display="none"
+    var personalInfor =  document.getElementById("PersonalInfo");
+    personalInfor.style.display="none"
+    var work =  document.getElementById("Work_experience");
+    work.style.display="none"
+    var references =  document.getElementById("References");
+    references.style.display="Block"
+    var education =  document.getElementById("Education");
+    education.style.display="none"
+}
